4e453c21a6d724498e8cbd8c77c6680fde349be8
tab <tab@0x09.net>
2026-02-16 16:21:48 -0500

    Use FUSE's option parsing system

--- src/listfs.c
+++ src/listfs.c
@@ -210,25 +210,50 @@ static struct fuse_operations listfs_ops = {
 #endif
 };
 
-int main(int argc, char* argv[]) {
-	// boilerplate from hfsfuse
-	// cheat a lot with option parsing to stay within the fuse_main high level API
-	if(argc < 3 || !strcmp(argv[1],"-h")) {
-		fuse_main(2,((char*[]){"listfs","-h"}),NULL,NULL);
+
+static struct fuse_opt listfs_opts[] = {
+	FUSE_OPT_KEY("-h",0),
+	FUSE_OPT_KEY("--help",0),
+	FUSE_OPT_END
+};
+
+static int listfs_opt_proc(void* data, const char* arg, int key, struct fuse_args* args) {
+	const char** device = data;
+	if(key == FUSE_OPT_KEY_NONOPT && !*device) {
+		*device = strdup(arg);
 		return 0;
 	}
+	else if(key == 0) {
+		fuse_opt_add_arg(args,"-h");
+		fuse_main(args->argc,args->argv,NULL,NULL);
+		fuse_opt_free_args(args);
+		exit(0);
+	}
+	return 1;
+}
 
-	const char opts[] = "-oro,allow_other,use_ino,subtype=list,fsname=";
-	const char* device = argv[argc-2];
-	char* mount = argv[argc-1];
-	char* argv2[argc+2];
-	argv2[0] = "listfs";
-	argv2[1] = mount;
-	argv2[2] = strcat(strcpy(malloc(strlen(opts)+strlen(device)+1),opts),device);
-	argv2[3] = "-s";
-	for(int i = 1; i < argc-2; i++)
-		argv2[i+3] = argv[i];
-	argv2[argc+1] = NULL;
+int main(int argc, char* argv[]) {
+	struct fuse_args args = FUSE_ARGS_INIT(argc, argv);
+
+	const char* device = NULL;
+
+	if(fuse_opt_parse(&args, &device, listfs_opts, listfs_opt_proc) == -1)
+		return 1;
+
+	char* fsname = malloc(strlen("fsname=") + strlen(device) + 1);
+	if(!fsname)
+		goto opt_err;
+	strcpy(fsname, "fsname=");
+	strcat(fsname, device);
+
+	char* opts = NULL;
+	fuse_opt_add_opt(&opts, "ro");
+	fuse_opt_add_opt(&opts, "use_ino");
+	fuse_opt_add_opt(&opts, "subtype=list");
+	fuse_opt_add_opt_escaped(&opts, fsname);
+	fuse_opt_add_arg(&args, "-o");
+	fuse_opt_add_arg(&args, opts);
+	fuse_opt_add_arg(&args, "-s");
 
 	struct btree root = {"/"};
 	FILE* f = fopen(device, "r");
@@ -258,7 +283,10 @@ int main(int argc, char* argv[]) {
 	}
 	fclose(f);
 
-	int ret = fuse_main(sizeof(argv2)/sizeof(*argv2)-1,argv2,&listfs_ops,&root);
+	int ret = fuse_main(args.argc,args.argv,&listfs_ops,&root);
+
+opt_err:
+	fuse_opt_free_args(&args);
 
 	return ret;
 }
